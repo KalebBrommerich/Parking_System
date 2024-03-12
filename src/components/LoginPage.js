df2c21a75f2f6c1b6f189a29dd8b20e8992d0fae
= <=>
2024-03-11 19:14:19 -0500

    changed login page to reflect using starID

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -15,13 +15,12 @@ const LoginPage = () => {
             <div className="container">
                 <h2>Login</h2>
                 <form action="Login" onSubmit={submitForm}>
-                    <label for="email">Email:</label>
+                    <label for="email">StarID:</label>
                     <input type="text" id="email" name="email" required />
                     <label for="password">Password:</label>
                     <input type="password" id="password" name="password" required/>
-                    <a href="forgotpassword.html" class="forgotpassword-link">Forgot Password?</a>
                     <hr/>
-                    <a href="demo1.html" class="create-account">Create Account</a>
+                    <a href="forgotpassword.html" class="forgotpassword-link">Forgot Password?</a>
                     <hr/>
                     <button type="submit" background-color="blue">Login</button>
                 </form>
